874017176b6848e332f9c94e76e2960397bfb3ca
れみろう <remyroez+github@gmail.com>
2019-10-15 21:27:11 +0900

    :pencil: 最初に Castle シーンに遷移するように対応

--- game/src/scenes/InGame.lua
+++ game/src/scenes/InGame.lua
@@ -3,39 +3,39 @@ local class = require 'middleclass'
 local lume = require 'lume'
 
 -- 基底クラス
-local Scene = require 'Scene'
+local Super = require 'Scene'
 
 -- インゲーム クラス
-local InGame = class('InGame', Scene)
+local Scene = class('InGame', Super)
 
 -- 初期化
-function InGame:initialize(t)
-    Scene.initialize(self)
+function Scene:initialize(t)
+    Super.initialize(self)
 
     self.app = t.app or {}
 end
 
 -- 破棄
-function InGame:destroy()
+function Scene:destroy()
 end
 
 -- 更新
-function InGame:update(dt)
+function Scene:update(dt)
 end
 
 -- 描画
-function InGame:draw()
-    love.graphics.print('Hello, InGame Scene!', 0, 100)
+function Scene:draw()
+    --love.graphics.print('Hello, InGame Scene!', 0, 100)
 end
 
 -- キー入力
-function InGame:keypressed(key, scancode, isrepeat)
+function Scene:keypressed(key, scancode, isrepeat)
     self:popScene()
 end
 
 -- マウス入力
-function InGame:mousepressed(x, y, button, istouch, presses)
+function Scene:mousepressed(x, y, button, istouch, presses)
     self:popScene()
 end
 
-return InGame
+return Scene
