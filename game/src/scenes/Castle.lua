3af59db14978ec8b9d08e6f03be6531132ae822e
れみろう <remyroez+github@gmail.com>
2019-10-15 21:26:40 +0900

    :sparkles: 城と迷宮の各シーンを追加

---
+++ game/src/scenes/Castle.lua
@@ -0,0 +1,58 @@
+
+local class = require 'middleclass'
+local lume = require 'lume'
+
+-- 基底クラス
+local Super = require 'Scene'
+
+-- クラス
+local Scene = class('Castle', Super)
+
+-- 初期化
+function Scene:initialize(t)
+    Super.initialize(self)
+
+    self.app = t.app or {}
+
+    self.text = '[Castle]\n'
+    self.text = self.text .. 'G)ilgamesh\'s Tavern\n'
+    self.text = self.text .. 'A)dventurer\'s Inn\n'
+    self.text = self.text .. 'T)emple of Cant\n'
+    self.text = self.text .. 'B)oltac\'s Trading Post\n'
+    self.text = self.text .. 'E)dge of Town\n'
+end
+
+-- 破棄
+function Scene:destroy()
+end
+
+-- 更新
+function Scene:update(dt)
+end
+
+-- 描画
+function Scene:draw()
+    love.graphics.print(self.text, 0, 0)
+end
+
+-- キー入力
+function Scene:keypressed(key, scancode, isrepeat)
+    if key == 'g' then
+        self:pushScene(require 'scenes.Tavern' { app = self.app })
+    elseif key == 'a' then
+        self:pushScene(require 'scenes.Inn' { app = self.app })
+    elseif key == 't' then
+        self:pushScene(require 'scenes.Temple' { app = self.app })
+    elseif key == 'b' then
+        self:pushScene(require 'scenes.Shop' { app = self.app })
+    elseif key == 'e' then
+        self:swapScene(require 'scenes.Edge' { app = self.app })
+    end
+end
+
+-- マウス入力
+function Scene:mousepressed(x, y, button, istouch, presses)
+    --self:popScene()
+end
+
+return Scene
---
+++ game/src/scenes/Inn.lua
@@ -0,0 +1,41 @@
+
+local class = require 'middleclass'
+local lume = require 'lume'
+
+-- 基底クラス
+local Super = require 'Scene'
+
+-- クラス
+local Scene = class('Inn', Super)
+
+-- 初期化
+function Scene:initialize(t)
+    Super.initialize(self)
+
+    self.app = t.app or {}
+end
+
+-- 破棄
+function Scene:destroy()
+end
+
+-- 更新
+function Scene:update(dt)
+end
+
+-- 描画
+function Scene:draw()
+    love.graphics.print('Inn', 0, 0)
+end
+
+-- キー入力
+function Scene:keypressed(key, scancode, isrepeat)
+    self:popScene()
+end
+
+-- マウス入力
+function Scene:mousepressed(x, y, button, istouch, presses)
+    self:popScene()
+end
+
+return Scene
