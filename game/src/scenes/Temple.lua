8eb4765e5d229edc007a3888886f5eeaef68ffd9
れみろう <remyroez+github@gmail.com>
2019-10-17 21:52:11 +0900

    :pencil: 寺院の仮フローを実装

--- game/src/scenes/Temple.lua
+++ game/src/scenes/Temple.lua
@@ -5,8 +5,11 @@ local lume = require 'lume'
 -- 基底クラス
 local Super = require 'Scene'
 
--- インゲーム クラス
+-- クラス
 local Scene = class('Temple', Super)
+
+-- 組み込み
+Scene:include(require 'stateful')
 
 -- 初期化
 function Scene:initialize(t)
@@ -25,17 +28,59 @@ end
 
 -- 描画
 function Scene:draw()
-    love.graphics.print('Temple', 0, 0)
+    love.graphics.print(
+        '[Temple of Cant]\n\n' ..
+        'Welcome to The Temple of cant\n' ..
+        'Who (letter) shall we help ?\n' ..
+        'ENTER to leave'
+        , 0, 0
+    )
 end
 
 -- キー入力
 function Scene:keypressed(key, scancode, isrepeat)
-    self:popScene()
+    local target = string.match(key, '^%a$')
+    if target then
+        self:pushState('Tithe', target)
+    elseif key == 'return' then
+        self:popScene()
+    end
 end
 
 -- マウス入力
 function Scene:mousepressed(x, y, button, istouch, presses)
-    self:popScene()
+    --self:popScene()
+end
+
+-- ステート追加
+local Tithe = Scene:addState 'Tithe'
+
+-- ステート開始時
+function Tithe:enteredState(target)
+    print(target)
+    self.tithe = {
+        target = target,
+    }
+end
+
+-- 描画
+function Tithe:draw()
+    love.graphics.print(
+        '[Temple of Cant]\n\n' ..
+        'The donation will be ' .. 1750 .. '\n' ..
+        'Who (#) will tithe ?\n' ..
+        'ENTER to leave'
+        , 0, 0
+    )
+end
+
+-- キー入力
+function Tithe:keypressed(key, scancode, isrepeat)
+    if key == '1' or key == '2' or key == '3' or key == '4' or key == '5' or key == '6' then
+        print('* Cheap apostates! Out! *')
+    elseif key == 'return' then
+        self:popState()
+    end
 end
 
 return Scene
