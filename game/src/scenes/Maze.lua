8cde78af1e288bf76e5032d3b250a52aba159638
れみろう <remyroez+github@gmail.com>
2019-10-17 19:39:48 +0900

    :pencil: 迷宮の仮フローを実装

--- game/src/scenes/Maze.lua
+++ game/src/scenes/Maze.lua
@@ -25,17 +25,34 @@ end
 
 -- 描画
 function Scene:draw()
-    love.graphics.print('Maze', 0, 0)
+    love.graphics.print(
+        '[Maze]\n' ..
+        'C)amp\n' ..
+        'S)tatus\n' ..
+        'I)nspect\n' ..
+        'O)ff',
+        0, 0
+    )
 end
 
 -- キー入力
 function Scene:keypressed(key, scancode, isrepeat)
-    self:popScene()
+    if key == 'c' then
+        self:pushScene(require 'scenes.Camp' { app = self.app })
+    elseif key == 's' then
+        print('Status')
+    elseif key == 'i' then
+        print('Inspect')
+    elseif key == 'o' then
+        print('Off')
+    elseif key == 'return' then
+        self:swapScene(require 'scenes.Castle' { app = self.app })
+    end
 end
 
 -- マウス入力
 function Scene:mousepressed(x, y, button, istouch, presses)
-    self:popScene()
+    --self:popScene()
 end
 
 return Scene
