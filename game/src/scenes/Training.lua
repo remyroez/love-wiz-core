0fb24995a75a4fac336d6ae6a61d1b81708bb3fb
れみろう <remyroez+github@gmail.com>
2019-10-18 22:45:53 +0900

    :pencil: 訓練場の仮フローを実装

--- game/src/scenes/Training.lua
+++ game/src/scenes/Training.lua
@@ -8,6 +8,9 @@ local Super = require 'Scene'
 -- インゲーム クラス
 local Scene = class('Training', Super)
 
+-- 組み込み
+Scene:include(require 'stateful')
+
 -- 初期化
 function Scene:initialize(t)
     Super.initialize(self)
@@ -25,17 +28,90 @@ end
 
 -- 描画
 function Scene:draw()
-    love.graphics.print('Training', 0, 0)
+    love.graphics.print(
+        '[Training Grounds]\n\n' ..
+        'C)reate a character\n' ..
+        'I)nspect a character\n' ..
+        'R)oster of characters\n' ..
+        'L)eave'
+        , 0, 0
+    )
 end
 
 -- キー入力
 function Scene:keypressed(key, scancode, isrepeat)
-    self:popScene()
+    if key == 'c' then
+        self:pushState 'Create'
+    elseif key == 'i' then
+        self:pushState 'Inspect'
+    elseif key == 'r' then
+        self:pushState 'Roster'
+    elseif key == 'l' or key == 'return' then
+        self:popScene()
+    end
 end
 
 -- マウス入力
 function Scene:mousepressed(x, y, button, istouch, presses)
-    self:popScene()
+    --self:popScene()
+end
+
+-- ステート追加
+local Create = Scene:addState 'Create'
+
+-- 描画
+function Create:draw()
+    love.graphics.print(
+        '[Training Grounds]\n\n' ..
+        'Create'
+        , 0, 0
+    )
+end
+
+-- キー入力
+function Create:keypressed(key, scancode, isrepeat)
+    if key == 'return' then
+        self:popState()
+    end
+end
+
+-- ステート追加
+local Inspect = Scene:addState 'Inspect'
+
+-- 描画
+function Inspect:draw()
+    love.graphics.print(
+        '[Training Grounds]\n\n' ..
+        '(LETTER) SELECTS, ENTER TO LEAVE\n\n' ..
+        'A) LLEW'
+        , 0, 0
+    )
+end
+
+-- キー入力
+function Inspect:keypressed(key, scancode, isrepeat)
+    if key == 'return' then
+        self:popState()
+    end
+end
+
+-- ステート追加
+local Roster = Scene:addState 'Roster'
+
+-- 描画
+function Roster:draw()
+    love.graphics.print(
+        '[Training Grounds]\n\n' ..
+        'Press ENTER'
+        , 0, 0
+    )
+end
+
+-- キー入力
+function Roster:keypressed(key, scancode, isrepeat)
+    if key == 'return' then
+        self:popState()
+    end
 end
 
 return Scene
