7d908071811ee4367606127fdcdf4b91af70cfb9
れみろう <remyroez+github@gmail.com>
2019-10-20 12:55:19 +0900

    :pencil: キャラクタースキーマを調整

--- game/src/schemas/Character.lua
+++ game/src/schemas/Character.lua
@@ -7,13 +7,18 @@ local Schema = class 'Character'
 -- 初期化
 function Schema:initialize(t)
     self.name = t.name or ''
+
     self.password = t.password or ''
-    self.alignment = t.alignment or ''
+
     self.race = t.race or ''
+
     self.class = t.class or ''
 
-    self.level = t.level or 1
     self.age = t.age or 0
+
+    self.status = t.status or ''
+
+    self.alignment = t.alignment or ''
 
     self.attribute = t.attribute or {}
     self.attribute.strength = self.attribute.strength or 0
@@ -26,23 +31,32 @@ function Schema:initialize(t)
     self.bonus = t.bonus or 0
 
     self.gold = t.gold or 0
+
+    self.items = t.items or {}
+
     self.exp = t.exp or 0
+    self.level = t.level or 0
+
     self.hp = t.hp or 0
     self.hpmax = t.hpmax or 0
-
-    self.ac = t.ac or 0
-
-    self.status = t.status or ''
-
-    self.mp = t.mp or {}
-    self.mp.mage = self.mp.mage or {}
-    self.mp.priest = self.mp.priest or {}
 
     self.spells = t.spells or {}
     self.spells.mage = self.spells.mage or {}
     self.spells.priest = self.spells.priest or {}
 
-    self.items = t.items or {}
+    self.mp = t.mp or {}
+    self.mp.mage = self.mp.mage or {}
+    self.mp.priest = self.mp.priest or {}
+
+    self.ac = t.ac or 0
+
+    self.whereabouts = t.whereabouts or {}
+    self.whereabouts.place = self.whereabouts.place or ''
+    self.whereabouts.x = self.whereabouts.x or 0
+    self.whereabouts.y = self.whereabouts.y or 0
+    self.whereabouts.level = self.whereabouts.level or ''
+
+    self.titles = t.titles or {}
 end
 
 return Schema
