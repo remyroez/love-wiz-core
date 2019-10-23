ba110115dfa6f2ee2547845cb0b1be651ac3e4a2
れみろう <remyroez+github@gmail.com>
2019-10-23 21:17:11 +0900

    :pencil: ダイススキーマにロール値，最大値，最小値を返すメソッドを追加

--- game/src/schemas/Dice.lua
+++ game/src/schemas/Dice.lua
@@ -11,4 +11,26 @@ function Schema:initialize(t)
     self.modifier = t.modifier or 0
 end
 
+-- ダイスロール
+function Schema:throw(randomizer)
+    randomizer = randomizer or math.random
+
+    local value = self.modifier
+    for i = 1, self.roll do
+        value = value + randomizer(self.side)
+    end
+
+    return value
+end
+
+-- 最大値
+function Schema:max()
+    return self.side * self.roll + self.modifier
+end
+
+-- 最低値
+function Schema:min()
+    return 1 * self.roll + self.modifier
+end
+
 return Schema
