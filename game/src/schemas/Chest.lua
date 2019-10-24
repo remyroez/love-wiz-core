72688d9119c08060216e433fcaf5780d18b433fb
れみろう <remyroez+github@gmail.com>
2019-10-24 20:31:41 +0900

    :sparkles: 宝箱スキーマと宝スキーマの繋ぎこみ，宝箱獲得メソッドの追加

--- game/src/schemas/Chest.lua
+++ game/src/schemas/Chest.lua
@@ -4,16 +4,42 @@ local class = require 'middleclass'
 -- スキーマ
 local Schema = class 'Chest'
 
+-- クラス
+local Treasure = require 'schemas.Treasure'
+
 -- 初期化
 function Schema:initialize(t)
-    self.items = t.items or {}
+    t = t or {}
 
-    self.gold = t.gold or {}
-    self.gold.probability = self.gold.probability or 0
-    self.gold.min = self.gold.min or 0
-    self.gold.max = self.gold.max or 0
+    -- お金
+    self.gold = Treasure(t.gold)
 
+    -- アイテム
+    self.items = {}
+    if t.items then
+        for _, item in ipairs(t.items) do
+            table.insert(self.items, Treasure(item))
+        end
+    end
+
+    -- 罠
     self.traps = t.traps or {}
+end
+
+function Schema:get(randomizer)
+    local treasures = {}
+
+    treasures.gold = self.gold:get(randomizer) or 0
+
+    treasures.items = {}
+    for _, item in ipairs(self.items) do
+        local treasure = item:get(randomizer)
+        if treasure then
+            table.insert(treasures.items, treasure)
+        end
+    end
+
+    return treasures
 end
 
 return Schema
