233cca4499ac831c79ebcb9a96e83dbee133857d
れみろう <remyroez+github@gmail.com>
2019-10-27 11:04:52 +0900

    :recycle: アーマークラススキーマのリファクタリング

--- game/src/schemas/ArmorClass.lua
+++ game/src/schemas/ArmorClass.lua
@@ -5,14 +5,13 @@ local lume = require 'lume'
 -- スキーマ
 local Schema = class 'ArmorClass'
 
--- インスタンスかどうか調べる
-function Schema.static.isInstance(self, v)
-    return lume.call(v.isInstanceOf, v, self)
-end
+-- 組み込み
+Schema:include(require 'Class')
+Schema:include(require 'Numeric')
 
 -- 初期化
 function Schema:initialize(v)
-    self.value = v or 0
+    self:initializeNumeric(v)
 end
 
 -- オーバーライド：文字列化
@@ -22,87 +21,6 @@ function Schema:__tostring()
     else
         return tostring(self.value)
     end
-end
-
--- オーバーライド：加算
-function Schema:__add(other)
-    if type(other) == 'number' then
-        return Schema(math.floor(self.value + other))
-    elseif Schema:isInstance(other) then
-        return Schema(math.floor(self.value + other.value))
-    else
-        error('Not an operational value')
-    end
-end
-
--- オーバーライド：減算
-function Schema:__sub(other)
-    if type(other) == 'number' then
-        return Schema(math.floor(self.value - other))
-    elseif Schema:isInstance(other) then
-        return Schema(math.floor(self.value - other.value))
-    else
-        error('Not an operational value')
-    end
-end
-
--- オーバーライド：乗算
-function Schema:__mul(other)
-    if type(other) == 'number' then
-        return Schema(math.floor(self.value * other))
-    elseif Schema:isInstance(other) then
-        return Schema(math.floor(self.value * other.value))
-    else
-        error('Not an operational value')
-    end
-end
-
--- オーバーライド：除算
-function Schema:__div(other)
-    if type(other) == 'number' then
-        return Schema(math.floor(self.value / other))
-    elseif Schema:isInstance(other) then
-        return Schema(math.floor(self.value / other.value))
-    else
-        error('Not an operational value')
-    end
-end
-
--- オーバーライド：剰余
-function Schema:__mod(other)
-    if type(other) == 'number' then
-        return Schema(math.floor(self.value % other))
-    elseif Schema:isInstance(other) then
-        return Schema(math.floor(self.value % other.value))
-    else
-        error('Not an operational value')
-    end
-end
-
--- オーバーライド：累乗
-function Schema:__pow(other)
-    if type(other) == 'number' then
-        return Schema(math.floor(self.value ^ other))
-    elseif Schema:isInstance(other) then
-        return Schema(math.floor(self.value ^ other.value))
-    else
-        error('Not an operational value')
-    end
-end
-
--- オーバーライド：比較
-function Schema:__eq(other)
-    return self.value == other.value
-end
-
--- オーバーライド：より小さい
-function Schema:__lt(other)
-    return self.value < other.value
-end
-
--- オーバーライド：以下
-function Schema:__le(other)
-    return self.value <= other.value
 end
 
 return Schema
