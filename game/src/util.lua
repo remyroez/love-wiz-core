1b0f7b62248d642bd63ce6b0348c92f1c0624bb7
れみろう <remyroez+github@gmail.com>
2019-11-03 21:32:37 +0900

    :sparkles: ユーティリティモジュールの追加

---
+++ game/src/util.lua
@@ -0,0 +1,62 @@
+
+local lume = require 'lume'
+
+-- ユーティリティ
+local util = {}
+
+-- クラスのインスタンスかどうか調べる
+function util.isInstance(class, instance)
+    return lume.call(instance.isInstanceOf, instance, class)
+end
+
+-- メタメソッド
+local freeze_metamethods = {
+    '__mode',
+    '__call',
+    '__tostring',
+    '__len',
+    '__pairs',
+    '__ipairs',
+    '__gc',
+    '__unm',
+    '__add',
+    '__sub',
+    '__mul',
+    '__div',
+    '__idiv',
+    '__mod',
+    '__pow',
+    '__concat',
+    '__band',
+    '__bor',
+    '__bxor',
+    '__bnot',
+    '__shl',
+    '__shr',
+    '__eq',
+    '__lt',
+    '__le',
+}
+
+-- 指定したテーブルをリードオンリーにする
+-- http://lua-users.org/wiki/ReadOnlyTables
+function util.freeze(t)
+    local mt = {
+        __index = t,
+        __newindex = function(...)
+            error("attempt to modify read-only table")
+        end,
+        __metatable = false
+    }
+    for _, meta in ipairs(freeze_metamethods) do
+        mt[meta] = t[meta]
+    end
+    return setmetatable({}, mt);
+end
+
+-- 有効なランダマイザを返す
+function util.randomizer(randomizer)
+    return randomizer or love.math.random
+end
+
+return util
