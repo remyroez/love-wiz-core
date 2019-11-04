4b6874eb14798dc40f2858c73d2e3ca1c7c9fdf7
れみろう <remyroez+github@gmail.com>
2019-11-04 16:18:45 +0900

    :sparkles: 注入関数に対応

--- game/src/di/container.lua
+++ game/src/di/container.lua
@@ -1,46 +1,68 @@
 
 local class = require 'middleclass'
 
+-- ＤＩコンテナ
 local container = class 'container'
 
+-- 初期化
 function container:initialize(t)
-    self.binded = t or {}
+    self.bindings = t or {}
 end
 
+-- オーバーライド：呼び出し
 function container:__call(...)
     return self:get(...)
 end
 
-function container:bind(key, value)
-    self.binded[key] = value
+-- バインド
+function container:bind(from, to)
+    self.bindings[from] = to
     return self
 end
 
-function container:get(key, ...)
-    local value = self.binded[key]
-    if type(value) == 'table' and type(value.new) == 'function' then
-        value = value:new(...)
-    elseif type(value) == 'function' then
-        value = value(...)
+-- 取得
+function container:get(from, ...)
+    local to = self.bindings[from]
+
+    -- インスタンス化
+    if type(to) == 'table' and type(to.new) == 'function' then
+        -- クラス
+        to = to:new(...)
+    elseif type(to) == 'function' then
+        -- 関数
+        to = to(...)
     end
-    if type(value) == 'table' and (value.dependencies or (value.class and value.class.dependencies)) then
-        local dependencies = value.dependencies or (value.class and value.class.dependencies)
+
+    -- テーブルなら依存関係の解決
+    if type(to) == 'table' and (to.dependencies or (to.class and to.class.dependencies)) then
+        -- 依存テーブルの取得
+        local dependencies = to.dependencies or (to.class and to.class.dependencies)
         if type(dependencies) == 'function' then
-            dependencies = dependencies(value, ...)
+            dependencies = dependencies(to, ...)
         end
         if type(dependencies) == 'table' then
             if dependencies[1] then
-                for k, dep in pairs(dependencies) do
-                    value[dep] = self:get(dep, ...)
+                -- 配列
+                for i, from in ipairs(dependencies) do
+                    if type(to[from]) == 'function' then
+                        to[from](to, self:get(from, ...))
+                    else
+                        to[from] = self:get(from, ...)
+                    end
                 end
             else
-                for k, dep in pairs(dependencies) do
-                    value[k] = self:get(dep, ...)
+                -- テーブル
+                for name, from in pairs(dependencies) do
+                    if type(to[name]) == 'function' then
+                        to[name](to, self:get(from, ...))
+                    else
+                        to[name] = self:get(from, ...)
+                    end
                 end
             end
         end
     end
-    return value
+    return to
 end
 
 return container
