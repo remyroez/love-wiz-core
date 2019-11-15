df99db75cd6a7457dfb7f72a862cd3ae620feedc
れみろう <remyroez+github@gmail.com>
2019-11-15 23:38:06 +0900

    :bento: 種族データ更新

--- game/assets/races.lua
+++ game/assets/races.lua
@@ -1,3 +1,72 @@
 return {
-
+    {
+        name = 'human',
+        statistics = {
+            strength = { value = 8, min = 3, max = 18 },
+            iq = { value = 8, min = 3, max = 18 },
+            piety = { value = 5, min = 3, max = 18 },
+            vitality = { value = 8, min = 3, max = 18 },
+            agility = { value = 8, min = 3, max = 18 },
+            luck = { value = 9, min = 3, max = 18 },
+        },
+        savingthrow = {
+            -1, 0, 0, 0, 0
+        }
+    },
+    {
+        name = 'elf',
+        statistics = {
+            strength = { value = 7, min = 3, max = 18 },
+            iq = { value = 10, min = 3, max = 18 },
+            piety = { value = 10, min = 3, max = 18 },
+            vitality = { value = 6, min = 3, max = 18 },
+            agility = { value = 9, min = 3, max = 18 },
+            luck = { value = 6, min = 3, max = 18 },
+        },
+        savingthrow = {
+            0, 0, -2, 0, 0
+        }
+    },
+    {
+        name = 'dwarf',
+        statistics = {
+            strength = { value = 10, min = 3, max = 18 },
+            iq = { value = 7, min = 3, max = 18 },
+            piety = { value = 10, min = 3, max = 18 },
+            vitality = { value = 10, min = 3, max = 18 },
+            agility = { value = 5, min = 3, max = 18 },
+            luck = { value = 6, min = 3, max = 18 },
+        },
+        savingthrow = {
+            0, 0, 0, -4, 0
+        }
+    },
+    {
+        name = 'gnome',
+        statistics = {
+            strength = { value = 7, min = 3, max = 18 },
+            iq = { value = 7, min = 3, max = 18 },
+            piety = { value = 10, min = 3, max = 18 },
+            vitality = { value = 8, min = 3, max = 18 },
+            agility = { value = 10, min = 3, max = 18 },
+            luck = { value = 7, min = 3, max = 18 },
+        },
+        savingthrow = {
+            0, -2, 0, 0, 0
+        }
+    },
+    {
+        name = 'hobbit',
+        statistics = {
+            strength = { value = 5, min = 3, max = 18 },
+            iq = { value = 7, min = 3, max = 18 },
+            piety = { value = 7, min = 3, max = 18 },
+            vitality = { value = 6, min = 3, max = 18 },
+            agility = { value = 10, min = 3, max = 18 },
+            luck = { value = 15, min = 3, max = 18 },
+        },
+        savingthrow = {
+            0, 0, 0, 0, -3
+        }
+    },
 }
