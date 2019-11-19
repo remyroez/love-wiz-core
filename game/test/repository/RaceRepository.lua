57dfd2befe855f1dc2a114b1d53261b3b313f75c
れみろう <remyroez+github@gmail.com>
2019-11-19 22:51:40 +0900

    :wastebasket: 不要なテストの削除

--- game/test/repository/RaceRepository.lua
+++
@@ -1,7 +0,0 @@
-local lust = require 'test.lust'
-local describe, it, expect = lust.describe, lust.it, lust.expect
-
-local RaceRepository = require 'repository.RaceRepository'
-
-describe('RaceRepository', function ()
-end)
